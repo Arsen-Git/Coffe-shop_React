a73d83ed9f1f9a47c32a491ea15cf7de67acdc6f
Arsen-Git <asermahambetov@gmail.com>
2022-11-01 17:08:27 +0100

    have created a main page without slider

--- src/App.js
+++ src/App.js
@@ -1,3 +1,193 @@
 export default function App() {
-  return <div className="wrapper"></div>;
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <div className="header__left">
+          <img
+            width={40}
+            height={40}
+            src="./img/sneakers__logo.svg"
+            alt="logo"
+          />
+          <div className="header__left__logoContainer">
+            <h2>REACT SNEAKERS</h2>
+            <p>Магазин найкращих кросівок</p>
+          </div>
+        </div>
+        <ul className="header__right">
+          <li className="header__right__cart">
+            <img width={20} height={20} src="./img/cart.svg" alt="cart" />
+            <p>1205 грн.</p>
+          </li>
+          <li className="header__right__favorite">
+            <img
+              width={20}
+              height={20}
+              src="./img/favourite.svg"
+              alt="favourite"
+            />
+          </li>
+          <li className="header__right__acc">
+            <img width={20} height={20} src="./img/acc.svg" alt="acc" />
+          </li>
+        </ul>
+      </header>
+      <main className="sneakers__content">
+        <div className="sneakers__head">
+          <h1 className="sneakers__title">Всі кросівки</h1>
+          <div className="sneakers__search">
+            <img width={14} height={14} src="./img/search.svg" alt="search" />
+            <input placeholder="Пошук..." />
+          </div>
+        </div>
+        <div className="sneakers__list">
+          <div className="sneakers">
+            <img
+              width={32}
+              height={32}
+              className="sneakers__favourite"
+              src="./img/sneakers__fav.svg"
+              alt="favorite"
+            />
+            <img
+              className="sneakers__img"
+              src="./img/sneakers1.jpg"
+              alt="sneakers"
+            />
+            <p>
+              <b>Kросівки</b> Puma X Aka Boku Future Rider
+            </p>
+            <div className="sneakers__footer">
+              <div className="sneakers__price">
+                <span>Ціна</span>
+                <p>16 499 грн.</p>
+              </div>
+              <img
+                width={32}
+                height={32}
+                src="./img/sneakers__inactive.svg"
+                alt="inactive"
+              />
+            </div>
+          </div>
+          <div className="sneakers">
+            <img
+              width={32}
+              height={32}
+              className="sneakers__favourite"
+              src="./img/sneakers__fav.svg"
+              alt="favorite"
+            />
+            <img
+              className="sneakers__img"
+              src="./img/sneakers1.jpg"
+              alt="sneakers"
+            />
+            <p>
+              <b>Kросівки</b> Puma X Aka Boku Future Rider
+            </p>
+            <div className="sneakers__footer">
+              <div className="sneakers__price">
+                <span>Ціна</span>
+                <p>16 499 грн.</p>
+              </div>
+              <img
+                width={32}
+                height={32}
+                src="./img/sneakers__inactive.svg"
+                alt="inactive"
+              />
+            </div>
+          </div>
+          <div className="sneakers">
+            <img
+              width={32}
+              height={32}
+              className="sneakers__favourite"
+              src="./img/sneakers__fav.svg"
+              alt="favorite"
+            />
+            <img
+              className="sneakers__img"
+              src="./img/sneakers1.jpg"
+              alt="sneakers"
+            />
+            <p>
+              <b>Kросівки</b> Puma X Aka Boku Future Rider
+            </p>
+            <div className="sneakers__footer">
+              <div className="sneakers__price">
+                <span>Ціна</span>
+                <p>16 499 грн.</p>
+              </div>
+              <img
+                width={32}
+                height={32}
+                src="./img/sneakers__inactive.svg"
+                alt="inactive"
+              />
+            </div>
+          </div>
+          <div className="sneakers">
+            <img
+              width={32}
+              height={32}
+              className="sneakers__favourite"
+              src="./img/sneakers__fav.svg"
+              alt="favorite"
+            />
+            <img
+              className="sneakers__img"
+              src="./img/sneakers1.jpg"
+              alt="sneakers"
+            />
+            <p>
+              <b>Kросівки</b> Puma X Aka Boku Future Rider
+            </p>
+            <div className="sneakers__footer">
+              <div className="sneakers__price">
+                <span>Ціна</span>
+                <p>16 499 грн.</p>
+              </div>
+              <img
+                width={32}
+                height={32}
+                src="./img/sneakers__inactive.svg"
+                alt="inactive"
+              />
+            </div>
+          </div>
+          <div className="sneakers">
+            <img
+              width={32}
+              height={32}
+              className="sneakers__favourite"
+              src="./img/sneakers__fav.svg"
+              alt="favorite"
+            />
+            <img
+              className="sneakers__img"
+              src="./img/sneakers1.jpg"
+              alt="sneakers"
+            />
+            <p>
+              <b>Kросівки</b> Puma X Aka Boku Future Rider
+            </p>
+            <div className="sneakers__footer">
+              <div className="sneakers__price">
+                <span>Ціна</span>
+                <p>16 499 грн.</p>
+              </div>
+              <img
+                width={32}
+                height={32}
+                src="./img/sneakers__inactive.svg"
+                alt="inactive"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
